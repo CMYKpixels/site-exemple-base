0d223ac9b058622ac3abdfa1fc8c0c1cdb03e5f1
Emmanuel Béziat <Emmanuel Bziat>
2016-06-02 12:37:01 +0200

    Menu burger mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
 $(function() {
+	// Ancres animées
 	$('.site-menu a').scrollOffset();
+
+	// Menu burger
+	$('.site-menu-burger').on('click', function() {
+		$(this).find('i').toggleClass('fa-close').toggleClass('fa-bars');
+
+		$('.site-menu-list').slideToggle();
+	});
 });
